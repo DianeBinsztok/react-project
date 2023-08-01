3327324f24de7a74b07de566b98e1806d625cd75
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-01 11:31:04 +0200

    feat: passing props to ListItem component

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,26 @@
 import '../styles/List.css';
 import ListItem from './ListItem';
+import '../styles/ListItem.css';
+import see from '../assets/icons/eye.png';
+
+// Chaque item doit avoir une clé (id) pour faciliter
+const listArray = [
+
+    {id:0, title: "Titre du premier item de liste", content:"Ceci est le contenu du premier item"},
+    {id:1, title: "Titre du second item de liste", content:"Contenu du second item"},
+    {id:2, title: "Troisième titre d'item de liste", content:"Ceci est le contenu du troisième item"},
+];
 
 function List() {
+
+    //La fonction fléchée retourne implicitement l'expression: pas besoin de "return"
+    const list = listArray.map((item, index)=>
+        <ListItem key={item.id} title= {item.title} content = {item.content}/>
+    );
+
     return (    
         <ul>
-            <ListItem />
-            <ListItem />
-            <ListItem />
+            {list}
         </ul> 
     );
    
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,12 +2,12 @@ import '../styles/ListItem.css';
 import see from '../assets/icons/eye.png';
 
 
-function ListItem() {
+function ListItem({id, title, content}) {
     return (    
-            <li className='listitem'>
-                <h3>Item de liste: titre</h3>
-                <p>Item de liste: contenu</p>
-                <img src={see}></img>
+            <li key={id} className='listitem'>
+                <h3>{title}</h3>
+                <p>{content}</p>
+                <img src={see} alt="Voir le contenu"/>
             </li>
     );
    
