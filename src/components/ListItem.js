a66082dd3e1ef00fa0f3a5c67e13902f91256961
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-06 15:33:58 +0200

    style: 'edit' icon

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,7 @@
 import '../styles/ListItem.css';
 import see from '../assets/icons/eye.png';
+import edit from '../assets/icons/edit.png';
+
 
 // Les props doivent être passées sous forme d'objet
 function ListItem({id, title, content}) {
@@ -7,7 +9,10 @@ function ListItem({id, title, content}) {
             <li key={id} className='listitem'>
                 <h3>{title}</h3>
                 <p>{content}</p>
+                <div className="listitem-icons">
                 <img src={see} alt="Voir le contenu"/>
+                <img src={edit} alt="Modifier"/>
+                </div>
             </li>
     );
    
